ab4b5501bb3bcf911952c688ac7d36ada948bff8
Nicolas Guerra <ngr@vera.com.uy>
2022-03-28 17:45:54 -0300

    TEST-corrección

--- mp.php
+++ mp.php
@@ -7,16 +7,16 @@
  */
 
 // Valores Definidos de las Configuraciones para el examen.
-require_once __DIR__ . '/cfg.php';
+require_once 'cfg.php';
 
 // SDK de Mercado Pago
-require __DIR__ . '/vendor/autoload.php';
+require 'vendor/autoload.php';
 
 use MercadoPago\SDK;
 
 // AccessToken Integrator_ID
-MercadoPago\SDK::setAccessToken('APP_USR-6317427424180639-042414-47e969706991d3a442922b0702a0da44-469485398');
-MercadoPago\SDK::setIntegratorId('dev_24c65fb163bf11ea96500242ac130004');
+SDK::setAccessToken('APP_USR-6317427424180639-042414-47e969706991d3a442922b0702a0da44-469485398');
+SDK::setIntegratorId('dev_24c65fb163bf11ea96500242ac130004');
 
 //------------------------------------------------------------------------------
 // Nuevo Pagador
--- notify.php
+++ notify.php
@@ -7,13 +7,13 @@
  */
 
 // SDK de Mercado Pago
-require __DIR__ . '/vendor/autoload.php';
+require 'vendor/autoload.php';
 
 use MercadoPago\SDK;
 
 // AccessToken Integrator_ID
-MercadoPago\SDK::setAccessToken('APP_USR-6317427424180639-042414-47e969706991d3a442922b0702a0da44-469485398');
-MercadoPago\SDK::setIntegratorId('dev_24c65fb163bf11ea96500242ac130004');
+SDK::setAccessToken('APP_USR-6317427424180639-042414-47e969706991d3a442922b0702a0da44-469485398');
+SDK::setIntegratorId('dev_24c65fb163bf11ea96500242ac130004');
 
 if (filter_input(INPUT_GET, 'type') == "payment") {
     $payment = MercadoPago\Payment::find_by_id(filter_input(INPUT_GET, 'data.id'));
